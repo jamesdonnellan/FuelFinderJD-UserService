e2007e695b219978b2a54fa157f541cc6e75ad8d
James Donnellan <G00434671@atu.ie>
2025-12-14 17:47:51 +0000

    Added a JSONProperty to ensure that id doesn't appear on swagger as it was causing error 400

--- UserService/src/main/java/ie/atu/userservice/userservice/Model/UserInfo.java
+++ UserService/src/main/java/ie/atu/userservice/userservice/Model/UserInfo.java
@@ -1,5 +1,6 @@
 package ie.atu.userservice.userservice.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -16,6 +17,7 @@ public class UserInfo
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id; // Primary Key
 
 
